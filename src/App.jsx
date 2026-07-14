98f319b3575e7ade44f671cda3094ae1e2f3e412
tammirsidevika <tammirsidevika05@gmail.com>
2026-07-14 15:30:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-const API_URL = "https://course-tracker-bd.onrender.com";
 function App() {
   return (
     <BrowserRouter>
