d3bcdc4b715c1e9618e500534db3c5b6ef16397a
tammirsidevika <tammirsidevika05@gmail.com>
2026-07-14 15:30:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-const API_URL = "https://course-tracker-bd.onrender.com";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
